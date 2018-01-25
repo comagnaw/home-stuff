e9b7b8ff0530d08f4eae6989308838b718f12e6f
comagnaw <comagnaw@gmail.com>
2018-01-25 06:58:52 -0500

    Update prompt and add bash script

---
+++ bin/sync2git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash  
+  
+cp ~/.bash_profile ~/home/sync/bash_profile
+cp ~/.bashrc ~/home/sync/bashrc
+cp ~/.vimrc ~/home/sync/vimrc
+cp ~/.screenrc ~/home/sync/screenrc_xterm-color
+cp ~/.git-completion.bash ~/home/sync/git-completion.bash
+cp ~/.powerline-shell.json ~/home/sync/powerline-shell.json
+cp ~/.gitconfig ~/home/sync/gitconfig
+cp ~/.gitconfig_include ~/home/sync/gitconfig_include
+cp ~/.pylintrc ~/home/sync/pylintrc
