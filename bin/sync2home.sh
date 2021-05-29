a85971c9aea35c63a1562e23b6cacaf956112647
comagnaw <comagnaw@gmail.com>
2021-05-29 18:43:30 -0400

    Updating for zsh support

--- bin/sync2home.sh
+++ bin/sync2home.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-cp ~/home/sync/bash_profile ~/.bash_profile
-cp ~/home/sync/bashrc ~/.bashrc
+cp ~/home/sync/zshrc ~/.zshrc
 cp ~/home/sync/vimrc ~/.vimrc
 cp ~/home/sync/screenrc_xterm-color ~/.screenrc
-cp ~/home/sync/git-completion.bash ~/.git-completion.bash
-cp ~/home/sync/powerline-shell.json ~/.powerline-shell.json
 cp ~/home/sync/gitconfig ~/.gitconfig
-cp ~/home/sync/pylintrc ~/.pylintrc
 
 cp ~/home/sync/homebrew.simonsimcity.update-notifier.plist ~/Library/LaunchAgents
 
